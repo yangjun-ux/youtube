4923cc8ef5bf4b82a700bfc76cfc3bc8d96363b8
yangjun-ux <nov16jun@gmail.com>
2020-08-11 17:54:35 +0900

    add login

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const { User } = require('./models/User')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
 //application/json
 app.use(bodyParser.json())
+
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -29,6 +32,31 @@ app.post('/register', (req, res) => {
         if(err) return res.json({success: false})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    //이메일이 있다면 비밀번호 확인
+    //비밀번호까지 맞다면 token 생성
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user)
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                // 토큰을 저장한다. 어디에?  쿠키, 로컬스토리지
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            })    
         })
     })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,43 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+    var user = this
+    //비밀번호를 암호화 시킨다.
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this
+    // jsonwebtoken 활용하여 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+    // user._id + 'secretToken' = token
+}
 
 const User = mongoose.model('User', userSchema)
 
