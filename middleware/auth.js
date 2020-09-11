11717646f85f7de1a75dc4ee5643bd5794404f31
yangjun-ux <nov16jun@gmail.com>
2020-09-11 16:38:01 +0900

    add: auth

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const { User } = require('../models/User');
+
+
+
+
+let auth = (req, res, next) => {
+    // 인증처리를 하는 곳
+    // 클라이언트 쿠키에서 토큰 가져오기
+    let token = req.cookies.x_auth;
+    // 토큰을 복호화 한 후 유저를 찾는다
+    User.findByToken(token, (err, user) => {
+        if(err) throw err;
+        if(!user) return res.json({ isAuth: false, error: true})
+
+        req.token = token;
+        req.user = user;
+        next();
+    })
+    // 유저가 있으면 인증 OK, 유저가 없으면 인증 NO
+}
+
+
+module.exports = { auth }
--- models/User.js
+++ models/User.js
@@ -34,14 +34,14 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.pre('save', function( next ) {
+userSchema.pre('save', function (next) {
     var user = this
     //비밀번호를 암호화 시킨다.
-    if(user.isModified('password')) {
-        bcrypt.genSalt(saltRounds, function(err, salt) {
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err)
-    
-            bcrypt.hash(user.password, salt, function(err, hash) {
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
                 if (err) return next(err)
                 user.password = hash
                 next()
@@ -52,25 +52,41 @@ userSchema.pre('save', function( next ) {
     }
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cb) {
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-        if(err) return cb(err)
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err)
         cb(null, isMatch)
     })
 }
 
-userSchema.methods.generateToken = function(cb) {
+userSchema.methods.generateToken = function (cb) {
     var user = this
     // jsonwebtoken 활용하여 token 생성
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
     user.token = token
-    user.save(function(err, user) {
-        if(err) return cb(err)
+    user.save(function (err, user) {
+        if (err) return cb(err)
         cb(null, user)
     })
     // user._id + 'secretToken' = token
 }
 
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        user.findOne({
+            "_id": decoded,
+            "token": token
+        }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
+
 const User = mongoose.model('User', userSchema)
 
-module.exports = { User }
+module.exports = {
+    User
+}
